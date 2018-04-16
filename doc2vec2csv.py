50c3ac8b6d0612efd39f358df459c9f45601cdef
Ehsan Montazeri <ehsan@d207-023-223-150.wireless.sfu.ca>
2018-04-16 01:30:50 -0700

    Adding the final doc2vec files

--- doc2vec2csv.py
+++ doc2vec2csv.py
@@ -10,11 +10,13 @@ from gensim.models.doc2vec import TaggedLineDocument
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE 
 #from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
 
 
 
 filename = 'Data/mergedDataSet.csv' #The complete file
-df = pd.read_csv(filename).iloc[0:1000]
+df = pd.read_csv(filename)
+#df = pd.read_csv(filename).iloc[0:1000]
 
 #Removing the stop words
 stop = open('stop_words.txt','r').read().split()
@@ -37,20 +39,30 @@ documents = TaggedLineDocument(commentsFileName) # Tags each sentence (0,1,2,...
 
 modelSize = 500 # Will represent each comment with a vector of size 500
 modelWindow = 8 
-model = Doc2Vec(documents, vector_size=modelSize, window=modelWindow, min_count=1, workers=4, dbow_words=1)
+model = Doc2Vec(documents, vector_size=modelSize, window=modelWindow, min_count=1, workers=4, dbow_words=1, epochs=20)
 
 #Creating a numpy array to keep the data and to be used by a machine learning model as the feature vector
 data = np.zeros((len(model.docvecs),len(model.docvecs[0])))
 for i in range(len(model.docvecs)):
     data[i]=model.docvecs[i]
 
-#Upsampling and saving the data as a CSV to load it into Spark later.
+
+#Creating validation sets
+X_train, X_val, y_train, y_val = train_test_split(data,target, test_size=0.2, random_state=0)
+
+#Upsamlping the training data
 sm = SMOTE(random_state=42)
-X_res, y_res = sm.fit_sample(data, target)
-dfData = pd.DataFrame(data=X_res,dtype=float)
-dfData['label']=y_res
-dfData.to_csv('Data/FeaturizedData.csv')
+X_res, y_res = sm.fit_sample(X_train, y_train)
+
+dfTrain = pd.DataFrame(data=X_res,dtype=float)
+dfTrain['label']=y_res
+dfTrain.to_csv('Data/FeaturizedDataTraining.csv')
+#dfTrain.to_csv('Data/FeaturizedDataTrainingSample.csv')
+dfVal = pd.DataFrame(data=X_val,dtype=float)
+dfVal['label']=y_val
+dfVal.to_csv('Data/FeaturizedDataValidation.csv')
+#dfVal.to_csv('Data/FeaturizedDataValidationSample.csv')
 
 
-
+print ('finished')
 
